add7424f55ed6754ee8a48672a73ec61cadb8ca5
German Gomez <grgomezu@gmail.com>
2021-03-07 10:25:10 -0700

    Added comments for clarity

--- Slope.cs
+++ Slope.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿/***************************************************************************************************************************************************************************************
+ *  @author: German Rafael Gomez Urbina
+ *  @email: grgomezu@gmail.com
+ *  
+ *  SQL Server user defined aggregate functions of the 
+ *  Linear Least-Squares Regression functions
+ *  
+ *  Based on:
+ *  
+ *  S. C. Chapra, Applied Numerical Methods with MATLAB for engineers and Scientists,
+ *  2nd Edition, McGraw Hill, 2008.
+ *  
+ *  Also based on Oracle's linear regression functions:
+ *  
+ *  https://docs.oracle.com/cd/B19306_01/server.102/b14200/functions132.htm
+ *  
+ * Using:  
+ * https://www.codeproject.com/articles/170061/custom-aggregates-in-sql-server
+ * https://docs.microsoft.com/en-us/sql/relational-databases/clr-integration-database-objects-user-defined-functions/clr-user-defined-aggregates-requirements?view=sql-server-ver15
+ * 
+ * 
+ * ***************************************************************************************************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlTypes;
 
-/* 
-    * Based on
-    * https://www.codeproject.com/articles/170061/custom-aggregates-in-sql-server
-    */
+
 [System.Serializable]
 [Microsoft.SqlServer.Server.SqlUserDefinedAggregate(
     Microsoft.SqlServer.Server.Format.Native,
@@ -19,11 +39,30 @@ using System.Data.SqlTypes;
     Name = "SLOPE")]
 public struct SLOPE
 {
+    /// <summary>
+    /// Number of rows
+    /// </summary>
     public SqlInt16 N { get; set; }
+    /// <summary>
+    /// Sxy = Sum(x*y) from 1 to N
+    /// </summary>
     private SqlDouble Sxy { get; set; }
+    /// <summary>
+    /// Sxx = Sum(x*x) from 1 to N
+    /// </summary>
     public SqlDouble Sxx { get; set; }
+    /// <summary>
+    /// Sx = Sum(x) from 1 to N
+    /// </summary>
     public SqlDouble Sx { get; set; }
+    /// <summary>
+    /// Sy = Sum(y) from 1 to N
+    /// </summary>
     public SqlDouble Sy { get; set; }
+    /// <summary>
+    /// Function for query processor to intialize the computation 
+    /// of the aggregation.
+    /// </summary>
     public void Init()
     {
         N = 0;
@@ -32,6 +71,11 @@ public struct SLOPE
         Sx = SqlDouble.Zero;
         Sy = SqlDouble.Zero;
     }
+    /// <summary>
+    /// Accumulation of the values being passed in
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
     public void Accumulate(SqlDouble x, SqlDouble y)
     {
         if (x.IsNull || y.IsNull)
@@ -45,6 +89,11 @@ public struct SLOPE
             Sy += y;
         }
     }
+    /// <summary>
+    /// Merge another instance of the aggregate class with current
+    /// instance.
+    /// </summary>
+    /// <param name="group"></param>
     public void Merge(SLOPE group)
     {
         if (
@@ -61,6 +110,10 @@ public struct SLOPE
             Sy += group.Sy;
         }
     }
+    /// <summary>
+    /// Completes the aggregate computation and returns the result.
+    /// </summary>
+    /// <returns>The result of the aggregation</returns>
     public SqlDouble Terminate()
     {
         return 
